e7a5d9ea7c5f5472d4b3c9e3a5a2b618983e000b
Victoria Clark <Victoria.Clark@iris.co.uk>
2026-02-18 21:01:31 +0000

    Finalize Todo Page

--- BlazorApp/Models/TodoItem.cs
+++ BlazorApp/Models/TodoItem.cs
@@ -1,25 +1,25 @@
-using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace BlazorApp.Models
 {
     public class TodoItem
     {
-        [JsonPropertyName("id")]
+        [JsonProperty("id")]
         public string Id { get; set; } = Guid.NewGuid().ToString();
 
-        [JsonPropertyName("partitionKey")]
+        [JsonProperty("partitionKey")]
         public string PartitionKey { get; set; } = "default";
 
-        [JsonPropertyName("title")]
+        [JsonProperty("title")]
         public string Title { get; set; } = string.Empty;
 
-        [JsonPropertyName("description")]
+        [JsonProperty("description")]
         public string Description { get; set; } = string.Empty;
 
-        [JsonPropertyName("isCompleted")]
+        [JsonProperty("isCompleted")]
         public bool IsCompleted { get; set; }
 
-        [JsonPropertyName("createdDate")]
+        [JsonProperty("createdDate")]
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
     }
 }
